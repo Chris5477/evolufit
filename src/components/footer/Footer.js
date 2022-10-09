8ff092ef98c2066c0dbe22e92c3cf77ef70df39c
Chris5477 <petitcolaschristopher@gmail.com>
2022-10-09 17:26:19 +0200

    add structure footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,7 +1,26 @@
-const Footer = () => {
-    return (
-        <footer data-testid="footer">footer</footer>
-    )
-}
+import facebook from "../../assets/icons/facebook.svg";
+import twitter from "../../assets/icons/twitter.svg";
+import instagram from "../../assets/icons/insta.svg";
+import youtube from "../../assets/icons/youtube.svg";
 
-export default Footer
+const Footer = () => {
+  return (
+    <footer data-testid='footer' className='container'>
+      <h2 className='main-title'>Evolu'fit</h2>
+      <div className='container-footer container'>
+        <ul>
+          <li>Qui sommes-nous </li>
+          <li>Nous contacter</li>
+          <li>Conditions générales d'utilisation </li>
+        </ul>
+        <div className='networks'>
+          <img src={facebook} alt='facebook' />
+          <img src={youtube} alt='youtube' />
+          <img src={twitter} alt='twitter' />
+          <img src={instagram} alt='instagram' />
+        </div>
+      </div>
+    </footer>
+  );
+};
+export default Footer;
