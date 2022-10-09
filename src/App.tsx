ac99c1d7f61fa0630545b97535b19bd0e0412652
Chris5477 <petitcolaschristopher@gmail.com>
2022-10-09 15:51:34 +0200

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 const App = () => {
-  return <div className='App'>Test</div>;
+  return (
+    <div className='App container'>
+      <BrowserRouter>
+        <Routes></Routes>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
