fe406a4bf15ca52aff2dbdb99c6439feb1dfc6e3
Chris5477 <petitcolaschristopher@gmail.com>
2022-10-10 14:33:57 +0200

    add structure to FormData

--- src/components/formData/FormData.js
+++ src/components/formData/FormData.js
@@ -1,5 +1,13 @@
-const FormData = () => {
-  return <div data-testid='input-form' className='form-data'></div>;
+const FormData = ({ label, valueInput, typeInput, handleChange, name }) => {
+  return (
+    <div data-testid='input-form' className='form-data container uppercase'>
+      <label htmlFor={label}>Votre {label} :</label>
+      <input className='uppercase' name={name} onChange={handleChange} id={label} type={typeInput} value={valueInput} />
+      <p className='error-validation'></p>
+    </div>
+  );
 };
-
+FormData.defaultProps = {
+  typeInput: "text",
+};
 export default FormData;
--- src/components/formData/FormData.spec.js
+++ src/components/formData/FormData.spec.js
@@ -2,13 +2,26 @@ import { render, screen } from "@testing-library/react";
 import FormData from "./FormData";
 
 describe("FormData", () => {
+  const props = {
+    label: "test",
+    valueInput: "",
+    handleChange: () => "test",
+    name: "test"
+  };
+
   beforeEach(() => {
-    render(<FormData label='test' valueInput='' typeInput='' handleChange={(e) => e.target.value} name='test' />);
+    render(<FormData {...props} />);
   });
 
   test("Should render the componant", () => {
-    const input = screen.getByTestId("input-form");
+    const form = screen.getByTestId("input-form");
+    expect(form).toBeInTheDocument();
+  });
 
-    expect(input).toBeInTheDocument();
+  test("Should add type text to input if the type isn't specified", () => {
+    const typeInput = document.querySelector("input").type;
+    if (props.type === undefined) {
+      expect(typeInput).toBe("text");
+    }
   });
 });
