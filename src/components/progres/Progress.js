1c1de0fb1ce1e6131a8096693d38632bab5698ac
Chris5477 <petitcolaschristopher@gmail.com>
2022-10-13 17:43:33 +0200

    add structure of componant

--- src/components/progres/Progress.js
+++ src/components/progres/Progress.js
@@ -1,7 +1,33 @@
-const Progress = () => {
-    return(
-        <div data-testid="progress" className="progress"></div>
-    )
-}
+export const getEvolutionWeight = (weight, initialWeight) => {
+  const newWeight = weight - initialWeight;
+  if (weight > initialWeight) {
+    return `+ ${newWeight} kg 🔺`;
+  } else if (weight < initialWeight) {
+    return `${newWeight} kg 🔻`;
+  } else {
+    return weight;
+  }
+};
 
-export default Progress
+const Progress = ({ label, unity, initialWeight, data }) => {
+  let getWeightProgression = null;
+  if (label === "poids") {
+    const newWeight = getEvolutionWeight(data, initialWeight);
+    getWeightProgression = `<p>${initialWeight !== newWeight && newWeight}</p>`;
+  }
+
+  return (
+    <section data-testid='progress' className='progress container'>
+      <h2 className='uppercase'></h2>
+      <div className='infos'>
+        <div className='indication'>
+          {label} ({unity})<p>{data}</p>
+          {getWeightProgression}
+        </div>
+        <div className='graphic'>{/* // graphic a definir */}</div>
+      </div>
+    </section>
+  );
+};
+
+export default Progress;
--- src/components/progres/Progress.spec.js
+++ src/components/progres/Progress.spec.js
@@ -1,11 +1,28 @@
-import { render, screen } from "@testing-library/react"
-import Progress from "./Progress"
+import { render, screen } from "@testing-library/react";
+import Progress, { getEvolutionWeight } from "./Progress";
 
 describe("Progress Componant", () => {
-    test("Should render the componant", () => {
-        render(<Progress />)
+  test("Should render the componant", () => {
+    render(<Progress />);
 
-        const componant = screen.getByTestId("progress")
-        expect(componant).toBeInTheDocument() 
-    })
-})
+    const componant = screen.getByTestId("progress");
+    expect(componant).toBeInTheDocument();
+  });
+});
+
+describe("getEvolutionWeight", () => {
+  let initialWeight = 85;
+
+  test("Should return the sum between initial weight and actual weight", () => {
+    const result = getEvolutionWeight(88, initialWeight);
+    expect(result).toBe(`+ 3 kg 🔺`);
+  });
+  test("Should return the difference between initial weight and actual weight", () => {
+    const result = getEvolutionWeight(82, initialWeight);
+    expect(result).toBe(`-3 kg 🔻`);
+  });
+  test("Should return the actual weight if initial weight and weight are equal", () => {
+    const result = getEvolutionWeight(85, initialWeight);
+    expect(result).toBe(85);
+  });
+});
