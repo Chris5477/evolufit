1ad0b24b3c00d26a4294f39501ce1b95cf7f0033
Chris5477 <petitcolaschristopher@gmail.com>
2022-10-20 12:42:44 +0200

    fix bugs

--- src/components/tabs/Tabs.js
+++ src/components/tabs/Tabs.js
@@ -2,12 +2,20 @@ import profil from "../../assets/icons/profil.svg";
 import cardio from "../../assets/icons/cardio.svg";
 import muscu from "../../assets/icons/muscu.svg";
 import planning from "../../assets/icons/planning.svg";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const Tabs = () => {
   const url = window.location.pathname;
   const [pathname, setPathname] = useState(url);
+
+  const backHome = () => setPathname("/");
+
+  useEffect(() => {
+    document.querySelector(".top-bar h1").addEventListener("click", backHome);
+
+    return () => document.querySelector(".top-bar h1").removeEventListener("click", backHome);
+  });
 
   return (
     <nav data-testid='tabs' className='tabs-navigation'>
